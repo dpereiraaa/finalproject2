20c4478fcc384d013e84cfb0059c30b4295d2a97
David <ddaarr@live.com.pt>
2021-11-19 14:57:45 +0000

    test

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,6 +5,7 @@ const router = require('express').Router();
 // GET  /signup
 router.get("/signup", (req, res) => {
     res.render("auth/signup-form");
+    console.log("hkfdkhytd");
   });
 
 module.exports = router;
