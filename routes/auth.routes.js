f8888fc390a69dd7688ea13046899f376b9f7c87
Raissa Damasceno <raissa.cod@gmail.com>
2021-11-22 10:35:17 +0000

    Update Signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,7 +5,6 @@ const router = require('express').Router();
 // GET  /signup
 router.get("/signup", (req, res) => {
     res.render("auth/signup-form");
-    console.log("hkfdkhytd");
   });
 
 module.exports = router;
